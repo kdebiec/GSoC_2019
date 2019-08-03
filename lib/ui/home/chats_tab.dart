dc1630f54e17a50b911ddefaefc4c929447ae1db
Konrad Dębiec <konradd@tutanota.com>
2019-08-03 23:50:48 +0200

    This tab now shows subcribed chats

--- lib/ui/home/chats_tab.dart
+++ lib/ui/home/chats_tab.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 import 'package:retroshare/ui/person_delegate.dart';
 import 'package:retroshare/model/home_tabs.dart';
 import 'package:retroshare/common/styles.dart';
+
+import 'package:retroshare/model/chat.dart';
+import 'package:retroshare/redux/model/app_state.dart';
 
 class ChatsTab extends StatelessWidget {
   @override
@@ -15,72 +19,83 @@ class ChatsTab extends StatelessWidget {
           SliverOverlapInjector(
             handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
           ),
-          SliverPadding(
-            padding:
-                const EdgeInsets.only(left: 8, top: 8, right: 16, bottom: 8),
-            sliver: SliverFixedExtentList(
-              itemExtent: personDelegateHeight,
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  if (index == 0) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pushNamed('/discover_chats');
-                      },
-                      child: Container(
-                        height: personDelegateHeight,
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 10.0,
-                              spreadRadius: 0.0,
-                              offset: Offset(
-                                0.0,
-                                5.0,
-                              ),
-                            ),
-                          ],
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(appBarHeight / 3)),
-                          color: Colors.white,
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    EdgeInsets.symmetric(horizontal: 50 * 0.5),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                      'Discover public chats',
-                                      style: Theme.of(context).textTheme.body2,
-                                    ),
-                                  ],
+          StoreConnector<AppState, List<Chat>>(
+            converter: (store) => store.state.subscribedChats,
+            builder: (context, chatsList) {
+              return SliverPadding(
+                padding: const EdgeInsets.only(
+                    left: 8, top: 8, right: 16, bottom: 8),
+                sliver: SliverFixedExtentList(
+                  itemExtent: personDelegateHeight,
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      if (index == 0) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushNamed('/discover_chats');
+                          },
+                          child: Container(
+                            height: personDelegateHeight,
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 10.0,
+                                  spreadRadius: 0.0,
+                                  offset: Offset(
+                                    0.0,
+                                    5.0,
+                                  ),
                                 ),
-                              ),
+                              ],
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(appBarHeight / 3)),
+                              color: Colors.white,
                             ),
-                          ],
-                        ),
-                      ),
-                    );
-                  } else {
-                    final PersonDelegateData data =
-                        allPages.values.elementAt(0)[index - 1];
-                    return PersonDelegate(
-                      data: data,
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/room');
-                      },
-                    );
-                  }
-                },
-                childCount: allPages.values.elementAt(0).length + 1,
-              ),
-            ),
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 50 * 0.5),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          'Discover public chats',
+                                          style:
+                                              Theme.of(context).textTheme.body2,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      } else {
+                        final PersonDelegateData data = PersonDelegateData(
+                          name: chatsList[index - 1].chatName,
+                          mId: chatsList[index - 1].chatId.toString(),
+                          isRoom: true,
+                        );
+                        return PersonDelegate(
+                          data: data,
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/room', arguments: {'isRoom': true, 'chatData': chatsList[index - 1]});
+                          },
+                        );
+                      }
+                    },
+                    childCount: chatsList == null ? 1 : chatsList.length + 1,
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
