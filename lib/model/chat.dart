fc1b35e3e7dff69dd6f2bd185e9aa87257ba4ffe
Konrad Dębiec <konradd@tutanota.com>
2019-08-04 22:48:11 +0200

    Added Message model

--- lib/model/chat.dart
+++ lib/model/chat.dart
@@ -14,3 +14,20 @@ class Chat {
       this.isPublic,
       this.numberOfParticipants});
 }
+
+class Message {
+  String msgId;
+  String msgContent;
+  String authorName;
+  String authorId;
+  String receiveTime;
+  bool isSent;
+
+  Message(
+      {this.msgId,
+      this.msgContent,
+      this.authorName,
+      this.authorId,
+      this.receiveTime,
+      this.isSent});
+}
