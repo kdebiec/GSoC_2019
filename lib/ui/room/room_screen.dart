2d9e84b9f0266d435216651579f4f598d90c3d3a
Konrad Dębiec <konradd@tutanota.com>
2019-08-04 22:51:05 +0200

    Wrapped screen into SafeArea Widget, also hiding avatar image for rooms

--- lib/ui/room/room_screen.dart
+++ lib/ui/room/room_screen.dart
@@ -4,7 +4,12 @@ import 'package:retroshare/common/styles.dart';
 import 'package:retroshare/ui/room/messages_tab.dart';
 import 'package:retroshare/ui/room/room_friends_tab.dart';
 
+import 'package:retroshare/model/chat.dart';
+
 class RoomScreen extends StatefulWidget {
+  final bool isRoom;
+  final Chat chat;
+  RoomScreen({Key key, this.isRoom = false, this.chat}) : super(key: key);
   @override
   _RoomScreenState createState() => _RoomScreenState();
 }
@@ -15,7 +20,6 @@ class _RoomScreenState extends State<RoomScreen>
 
   final bool isOnline = true;
   final String profileImage = 'assets/profile.jpg';
-  final String name = 'Sandie Gloop';
 
   Animation<Color> _iconAnimation;
 
@@ -25,7 +29,7 @@ class _RoomScreenState extends State<RoomScreen>
     _tabController = new TabController(vsync: this, length: 2);
 
     _iconAnimation =
-        ColorTween(begin: Colors.black45, end: Colors.lightBlueAccent)
+        ColorTween(begin: Colors.black, end: Colors.lightBlueAccent)
             .animate(_tabController.animation);
   }
 
@@ -38,105 +42,111 @@ class _RoomScreenState extends State<RoomScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: appBarHeight + 32,
-            padding: const EdgeInsets.fromLTRB(8.0, 32.0, 16.0, 8.0),
-            child: Row(
-              children: <Widget>[
-                Container(
-                  width: personDelegateHeight,
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      size: 25,
-                    ),
-                    color: Color(0xFF9E9E9E),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-                Container(
-                  width: appBarHeight,
-                  height: appBarHeight,
-                  child: Stack(
-                    alignment: Alignment(-1.0, 0.0),
-                    children: <Widget>[
-                      Center(
-                        child: Container(
-                          height: appBarHeight * 0.8,
-                          width: appBarHeight * 0.8,
-                          decoration: BoxDecoration(
-                            border: null,
-                            color: Colors.lightBlueAccent,
-                            borderRadius: BorderRadius.circular(
-                                appBarHeight * 0.8 * 0.33),
-                            image: DecorationImage(
-                              fit: BoxFit.fitWidth,
-                              image: AssetImage(profileImage),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Visibility(
-                        visible: isOnline,
-                        child: Positioned(
-                          bottom: appBarHeight * 0.73,
-                          left: appBarHeight * 0.73,
-                          child: Container(
-                            height: appBarHeight * 0.25,
-                            width: appBarHeight * 0.25,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Colors.white,
-                                  width: appBarHeight * 0.03),
-                              color: Colors.lightGreenAccent,
-                              borderRadius: BorderRadius.circular(
-                                  appBarHeight * 0.3 * 0.5),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 10.0),
-                Expanded(
-                  child: Text(
-                    name,
-                    style: Theme.of(context).textTheme.body2,
-                  ),
-                ),
-                AnimatedBuilder(
-                  animation: _tabController.animation,
-                  builder: (BuildContext context, Widget widget) {
-                    return IconButton(
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: appBarHeight,
+              padding: const EdgeInsets.fromLTRB(8.0, 0.0, 16.0, 0.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: personDelegateHeight,
+                    child: IconButton(
                       icon: Icon(
-                        Icons.people,
+                        Icons.arrow_back,
                         size: 25,
                       ),
-                      color: _iconAnimation.value,
                       onPressed: () {
-                        _tabController.animateTo(1 - _tabController.index);
+                        Navigator.pop(context);
                       },
-                    );
-                  },
-                ),
-              ],
+                    ),
+                  ),
+                  Visibility(
+                    visible: !widget.isRoom,
+                    child: Container(
+                      width: appBarHeight,
+                      height: appBarHeight,
+                      child: Stack(
+                        alignment: Alignment(-1.0, 0.0),
+                        children: <Widget>[
+                          Center(
+                            child: Container(
+                              height: appBarHeight * 0.8,
+                              width: appBarHeight * 0.8,
+                              decoration: BoxDecoration(
+                                border: null,
+                                color: Colors.lightBlueAccent,
+                                borderRadius: BorderRadius.circular(
+                                    appBarHeight * 0.8 * 0.33),
+                                image: DecorationImage(
+                                  fit: BoxFit.fitWidth,
+                                  image: AssetImage(profileImage),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Visibility(
+                            visible: isOnline,
+                            child: Positioned(
+                              bottom: appBarHeight * 0.73,
+                              left: appBarHeight * 0.73,
+                              child: Container(
+                                height: appBarHeight * 0.25,
+                                width: appBarHeight * 0.25,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Colors.white,
+                                      width: appBarHeight * 0.03),
+                                  color: Colors.lightGreenAccent,
+                                  borderRadius: BorderRadius.circular(
+                                      appBarHeight * 0.3 * 0.5),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Expanded(
+                    child: Text(
+                      widget.isRoom ? widget.chat.chatName : '',
+                      style: Theme.of(context).textTheme.body2,
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: _tabController.animation,
+                    builder: (BuildContext context, Widget widget) {
+                      return IconButton(
+                        icon: Icon(
+                          Icons.people,
+                          size: 25,
+                        ),
+                        color: _iconAnimation.value,
+                        onPressed: () {
+                          _tabController.animateTo(1 - _tabController.index);
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: TabBarView(
-              controller: _tabController,
-              children: [
-                MessagesTab(),
-                RoomFriendsTab(),
-              ],
-            ),
-          )
-        ],
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  MessagesTab(chat: widget.chat),
+                  RoomFriendsTab(chat: widget.chat),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
