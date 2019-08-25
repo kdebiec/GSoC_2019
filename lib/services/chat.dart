a3a889a9c417b015cc026407e21d8899d92502aa
Konrad Dębiec <konradd@tutanota.com>
2019-08-25 23:08:28 +0200

    Added service to get lobby participants

--- lib/services/chat.dart
+++ lib/services/chat.dart
@@ -138,6 +138,29 @@ Future<List<Identity>> getLobbyParticipants(int lobbyId) async {
     'http://localhost:9092/rsMsgs/getChatLobbyInfo',
     headers: {
       HttpHeaders.authorizationHeader:
-      'Basic ' + base64.encode(utf8.encode('$authToken'))
+          'Basic ' + base64.encode(utf8.encode('$authToken'))
     },
     body: json.encode({
+      'id': lobbyId,
+    }),
+  );
+
+  List<Identity> ids = List<Identity>();
+
+  if (response.statusCode == 200) {
+    var gxsIds = json.decode(response.body)['info']['gxs_ids'];
+    for (int i = 0; i < gxsIds.length; i++) {
+      bool success = true;
+      Identity id;
+      do {
+        Tuple2<bool, Identity> tuple = await getIdDetails(gxsIds[i]['key']);
+        success = tuple.item1;
+        id = tuple.item2;
+      } while (!success);
+
+      ids.add(id);
+    }
+    return ids;
+  } else
+    throw Exception('Failed to load response');
+}
